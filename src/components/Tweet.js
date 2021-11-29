a91a564266075183cd2f532bba94d29b4c9d195f
Diogo Monteiro <diogopratasmonteiro@gmail.com>
2021-11-29 17:51:22 +0000

    lab done

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,37 +1,27 @@
-function Tweet() {
+import Actions from "./Actions";
+import Message from "./Message";
+import ProfileImage from "./ProfileImage";
+import User from "./User";
+import Timestamp from "./Timestamp";
+
+function Tweet(props) {
+  // console.log(props.tweet.user);
   return (
-    <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+    <div className='tweet'>
+      <ProfileImage image={props.tweet.user.image} />
 
-      <div className="body">
-        <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
-
-          <span className="timestamp">Nov 30, 2020</span>
+      <div className='body'>
+        <div className='top'>
+          <User name={props.tweet.user.name} handle={props.tweet.user.handle} />
+          {/* OR <User userData={props.tweet.user} /> and access each value on the User component */}
+          <Timestamp time={props.tweet.timestamp} />
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
-
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i class="far fa-comment"></i>
-          <i class="fas fa-retweet"></i>
-          <i class="far fa-heart"></i>
-          <i class="fas fa-share"></i>
-        </div>
+        <Message message={props.tweet.message} />
+        <Actions />
       </div>
 
-      <i class="fas fa-ellipsis-h"></i>
+      <i className='fas fa-ellipsis-h'></i>
     </div>
   );
 }
